f6eac5b1cd36c0c327fea3ec9b86af52484900be
Kenny Hammerlund <Kenny.hammerlund@gmail.com>
2018-06-14 00:14:11 -0500

    Initial Folder structure and index setup:

---
+++ imports/ui/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello TripRec</h1>
+        <h3> This is the rocket ship we blast off on</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
