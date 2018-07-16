e3137209af3440c4da5622c77811a207981d56fb
Kenny Hammerlund <Kenny.hammerlund@gmail.com>
2018-07-15 23:07:06 -0500

    Crash course

--- imports/ui/pages/Dashboard/index.js
+++ imports/ui/pages/Dashboard/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-
+import Image from "../../components/Image";
+import Navigation from "../Navigation";
 export class index extends Component {
   render() {
     return (
       <div>
+        <Navigation />
         <h1>This is the Dashboard</h1>
         <h2>52.87.223.21</h2>
       </div>
     );
   }
 }
-
-export default index;
---
+++ imports/ui/pages/Navigation/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class index extends Component {
+  render() {
+    return (
+      <div className="row top-nav">
+        <div className="col-xs-8 col-lg-10">
+          <h3>TripRec: App</h3>
+        </div>
+        <div className="col-xs-4 col-lg-2">Users Portal</div>
+      </div>
+    );
+  }
+}
+
+export default index;
