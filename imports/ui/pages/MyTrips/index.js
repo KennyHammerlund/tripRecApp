cf08fa5a7c30dd3feb8a7f08c6d3be8a20ac90f5
Justin <ow7128ch@metrostate.edu>
2018-07-31 23:38:56 -0500

    imported graphql

--- imports/ui/pages/MyTrips/index.js
+++ imports/ui/pages/MyTrips/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
 // import { Switch, Route } from "react-router-dom";
 
 const tripsQuery = gql`
