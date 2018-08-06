258b354416531117f6a44d21ad8266aa078998d2
Justin <ow7128ch@metrostate.edu>
2018-08-05 23:26:11 -0500

    updated myTrips

--- imports/ui/pages/MyTrips/index.js
+++ imports/ui/pages/MyTrips/index.js
@@ -31,17 +31,37 @@ export class index extends Component {
     
     return (
 
-      <div>
-      <div>
-        
-        <h2>This is the my trips page</h2>
+      <div className="flex flex-column m-b-20">
+        <div class="col-lg-8">
+          <div class="card-box">
+            <h4 class ="text-dark header-title m-t-0">Your Trips</h4>
+          
+            <p class="text-muted m-b-25 font-13">go out and complete more trips!</p>
+         
+            <div class = "table-responsive">
+              <table class ="table mb-0">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Title</th>
+                  <th>Picture</th>
+                  <th>Comments</th>
+                </tr>
+              </thead>
+            <tbody class = "centered">
+              <tr>1</tr>
+                <td>hard coded</td>
+                <td>hard coded</td>
+                <td>hard coded</td>
+              <tr>2</tr>
+              <tr>3</tr>
+              <tr>4</tr>
+              {user&& user.trips ? user.trips.map(trip => <Trip trip={trip} key={trip.id} />):null}
+            </tbody>
+          </table>
+          </div>
+          </div>
         </div>
-      
-        <table>
-          <tbody>
-            {user&& user.trips ? user.trips.map(trip => <Trip trip={trip} key={trip.id} />):null}
-          </tbody>
-        </table>
       </div>
     );
   }
