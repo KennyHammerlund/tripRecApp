46c2fb0bf2e76e9b8e1325f3b6b802ed286320a0
Kenny Hammerlund <Kenny.hammerlund@gmail.com>
2018-08-15 12:52:59 -0500

    add ssl

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -5,24 +5,24 @@ import { ApolloProvider } from "react-apollo";
 import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
-import { setContext } from 'apollo-link-context'
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import { AUTH_TOKEN } from '../../ui/constants'
+import { setContext } from "apollo-link-context";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { AUTH_TOKEN } from "../../ui/constants";
 import App from "../../ui/App";
 
 const httpLink = new HttpLink({
-  uri: "http://triprec-gql.herokuapp.com/graphql"
+  uri: "https://triprec-gql.herokuapp.com/graphql"
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem(AUTH_TOKEN)
+  const token = localStorage.getItem(AUTH_TOKEN);
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : ''
+      authorization: token ? `Bearer ${token}` : ""
     }
-  }
-})
+  };
+});
 
 const cache = new InMemoryCache();
 
@@ -33,31 +33,30 @@ const client = new ApolloClient({
 
 const theme = createMuiTheme({
   typography: {
-    fontSize: 25,
+    fontSize: 25
   },
   palette: {
     primary: {
-      light: '#8a6f67',
-      main: '#6d4c41',
-      dark: '#4c352d',
-      contrastText: '#fff',
+      light: "#8a6f67",
+      main: "#6d4c41",
+      dark: "#4c352d",
+      contrastText: "#fff"
     },
     secondary: {
-      light: '#33d375',
-      main: '#00c853',
-      dark: '#008c3a',
-      contrastText: '#000',
-    },
-  },
+      light: "#33d375",
+      main: "#00c853",
+      dark: "#008c3a",
+      contrastText: "#000"
+    }
+  }
 });
-
 
 Meteor.startup(() => {
   render(
     <ApolloProvider client={client}>
-      <MuiThemeProvider theme={theme}> 
+      <MuiThemeProvider theme={theme}>
         <App />
-      </MuiThemeProvider >
+      </MuiThemeProvider>
     </ApolloProvider>,
     document.getElementById("app")
   );
